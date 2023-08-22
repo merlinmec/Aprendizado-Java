7e3802aa30ec42eabe4445888bfc892da9f06f9e
João Manoel Carvalho <joaomanoel190701@gmail.com>
2023-08-22 09:35:15 -0300

    primeiros exercicios

---
+++ TaMec.java
@@ -0,0 +1,6 @@
+public class TaMec {
+    public static void main(String[] args) {
+        System.out.println("Minha primeira aplicação Java!\nE pulei uma linha hehehe :)");
+        System.out.println("Minha segunda linha de texto Java!");
+    }
+}
